15a48d82324ab10dbe935b23f117617cf89c0ffd
NushySC <ank.sampedro@gmail.com>
2019-03-03 19:49:10 +0100

    form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,23 @@
 
 @section ('content')
 
-<div class="display-4 p-3"></div>
+<div class="display-4 p-3">Create Post</div>
+
+{{-- {{ Form::open(array('url' => 'foo/bar')) }} --}}
+
+{!! Form::open(['action' => 'PostsController@store', 'method' => 'POST']) !!}
+
+<div class="form-group">
+    {{ Form::label('title', 'Title')}}
+    {{ Form::text('title', '', [ 'class' => 'form-control', 'placeholder' => 'title'])}}
+</div>
+
+<div class="form-group">
+        {{ Form::label('body', 'Body')}}
+        {{ Form::textarea('body', '', [ 'class' => 'form-control', 'placeholder' => 'body'])}}
+    </div>
+
+{!! Form::close() !!}
 
 
 @endsection
